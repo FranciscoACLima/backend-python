d44d02a16092a1cea254f93e1609d6fdd171047f
FranciscoACLima <franciscoaclima@outlook.com>
2021-04-21 13:14:05 -0300

    feat: Implementing others controllers

--- src/presenters/controlllers/find_user_controller.py
+++ src/presenters/controlllers/find_user_controller.py
@@ -17,44 +17,41 @@ class FindUserController:  # RouteInterface
 
         response = None
 
-        if http_request.query:
+        if not http_request.query:
+            # If no query in http_request
+            http_error = HttpErrors.error_400()
+            return HttpResponse(
+                status_code=http_error["status_code"], body=http_error["body"]
+            )
 
-            query_string_params = http_request.query.keys()
+        query_string_params = http_request.query.keys()
 
-            if "user_id" in query_string_params and "user_name" in query_string_params:
-                user_id = http_request.query["user_id"]
-                user_name = http_request.query["user_name"]
-                response = self.find_user_use_case.by_id_and_name(
-                    user_id=user_id, name=user_name
-                )
+        if "user_id" in query_string_params and "user_name" in query_string_params:
+            user_id = http_request.query["user_id"]
+            user_name = http_request.query["user_name"]
+            response = self.find_user_use_case.by_id_and_name(
+                user_id=user_id, name=user_name
+            )
 
-            elif (
-                "user_id" in query_string_params
-                and "user_name" not in query_string_params
-            ):
-                user_id = http_request.query["user_id"]
-                response = self.find_user_use_case.by_id(user_id=user_id)
+        elif (
+            "user_id" in query_string_params and "user_name" not in query_string_params
+        ):
+            user_id = http_request.query["user_id"]
+            response = self.find_user_use_case.by_id(user_id=user_id)
 
-            elif (
-                "user_id" not in query_string_params
-                and "user_name" in query_string_params
-            ):
-                user_name = http_request.query["user_name"]
-                response = self.find_user_use_case.by_name(name=user_name)
+        elif (
+            "user_id" not in query_string_params and "user_name" in query_string_params
+        ):
+            user_name = http_request.query["user_name"]
+            response = self.find_user_use_case.by_name(name=user_name)
 
-            else:
-                response = {"Success": False, "Data": None}
+        else:
+            response = {"Success": False, "Data": None}
 
-            if response["Success"] is False:
-                http_error = HttpErrors.error_422()
-                return HttpResponse(
-                    status_code=http_error["status_code"], body=http_error["body"]
-                )
+        if response["Success"] is False:
+            http_error = HttpErrors.error_422()
+            return HttpResponse(
+                status_code=http_error["status_code"], body=http_error["body"]
+            )
 
-            return HttpResponse(status_code=200, body=response["Data"])
-
-        # If no query in http_request
-        http_error = HttpErrors.error_400()
-        return HttpResponse(
-            status_code=http_error["status_code"], body=http_error["body"]
-        )
+        return HttpResponse(status_code=200, body=response["Data"])
--- src/presenters/controlllers/find_user_controller_test.py
+++ src/presenters/controlllers/find_user_controller_test.py
@@ -8,8 +8,8 @@ from .find_user_controller import FindUserController
 faker = Faker()
 
 
-def test_handle():
-    """ Testing Handle method """
+def test_route():
+    """ Testing Route method """
 
     find_user_use_case = FindUserSpy(UserRepositorySpy())
     find_user_controller = FindUserController(find_user_use_case)
@@ -34,8 +34,8 @@ def test_handle():
     assert response.body
 
 
-def test_handle_no_query_param():
-    """ Testing Handle method """
+def test_route_no_query_param():
+    """ Testing Route method """
 
     find_user_use_case = FindUserSpy(UserRepositorySpy())
     find_user_controller = FindUserController(find_user_use_case)
@@ -50,4 +50,25 @@ def test_handle_no_query_param():
 
     # Testing Output
     assert response.status_code == 400
+    assert "error" in response.body
+
+
+def test_route_query_params_error():
+    """ Testing Route method """
+
+    find_user_use_case = FindUserSpy(UserRepositorySpy())
+    find_user_controller = FindUserController(find_user_use_case)
+    http_request = HttpRequest(
+        query={"id": faker.random_number(), "name": faker.word()}
+    )
+
+    response = find_user_controller.route(http_request)
+
+    # Testing Inputs
+    assert find_user_use_case.by_id_and_name_param == {}
+    assert find_user_use_case.by_id_param == {}
+    assert find_user_use_case.by_name_param == {}
+
+    # Testing Output
+    assert response.status_code == 422
     assert "error" in response.body
